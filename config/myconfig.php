1aa4094efccb2ee055fea609b7b22cb78fef7363
pankaj <pankaj@vm-jenkins-new.com>
2020-12-14 16:50:35 +0530

    Admin theme setup
    Signed-off-by: pankaj <pankaj@vm-jenkins-new.com>

---
+++ config/myconfig.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+
+    'education'=>[
+        'B.Tech',
+        'B.A.',
+        'M.tech',
+        'Ll.B',
+        'MBBS'
+    ],
+
+    'occupation'=>[
+        'Doctor',
+        'Lawyer',
+        'Engineer',
+        'Accountant'
+    ],
+
+    'marrital'=>[
+        'Never Married',
+        'Married',
+        'Widow',
+        'Divorced'
+    ],
+
+
+];
