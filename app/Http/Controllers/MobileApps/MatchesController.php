365999563d00202dce486281f9b5a5b950ba2d85
PankajVm <p.ks11iitr@gmail.com>
2021-01-31 11:37:02 +0530

    firebase notifications

--- app/Http/Controllers/MobileApps/MatchesController.php
+++ app/Http/Controllers/MobileApps/MatchesController.php
@@ -16,30 +16,30 @@ class MatchesController extends Controller
 
         $profiles=Customer::select('id', 'name','image', 'dob')->where('id', '!=', $user->id);
 
-        if($user->pref_gender=='Male')
-            $profiles=$profiles->where('gender', 'Male');
-        else
-            $profiles=$profiles->where('gender', 'Female');
+//        if($user->pref_gender=='Male')
+//            $profiles=$profiles->where('gender', 'Male');
+//        else
+//            $profiles=$profiles->where('gender', 'Female');
+//
+//        if($user->from_height && $user->to_height){
+//            $profiles=$profiles->whereHas('height', function($height)use($user){
+//                $height->where('numeric_height', '>=', $user->from_height)->where('numeric_height', '<=', $user->to_height);
+//            });
+//        }
+//
+//        if($user->from_age){
+//            $profiles=$profiles
+//                ->whereNotNull('dob')
+//                ->where(DB::raw("DATEDIFF('".date('Y-m-d')."', dob ) / 365"), '>=', $user->from_age);
+//        }
+//
+//        if($user->to_age){
+//            $profiles=$profiles
+//            ->whereNotNull('dob')
+//                ->where(DB::raw("DATEDIFF('".date('Y-m-d')."', dob ) / 365"), '<=', $user->to_age);
+//        }
 
-        if($user->from_height && $user->to_height){
-            $profiles=$profiles->whereHas('height', function($height)use($user){
-                $height->where('numeric_height', '>=', $user->from_height)->where('numeric_height', '<=', $user->to_height);
-            });
-        }
-
-        if($user->from_age){
-            $profiles=$profiles
-                ->whereNotNull('dob')
-                ->where(DB::raw("DATEDIFF('".date('Y-m-d')."', dob ) / 365"), '>=', $user->from_age);
-        }
-
-        if($user->to_age){
-            $profiles=$profiles
-            ->whereNotNull('dob')
-                ->where(DB::raw("DATEDIFF('".date('Y-m-d')."', dob ) / 365"), '<=', $user->to_age);
-        }
-
-        $profiles=$profiles->paginate(4);
+        $profiles=$profiles->inRandomOrder()->paginate(50);
 
         return [
 
