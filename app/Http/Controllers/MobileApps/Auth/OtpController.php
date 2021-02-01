629bedd2b445401774e989fe103502f877f2c227
PankajVm <p.ks11iitr@gmail.com>
2021-02-01 17:12:41 +0530

    firebase notifications

--- app/Http/Controllers/MobileApps/Auth/OtpController.php
+++ app/Http/Controllers/MobileApps/Auth/OtpController.php
@@ -44,7 +44,7 @@ class OtpController extends Controller
         $user=Customer::where('mobile', $request->mobile)->first();
         if($user->status==0){
             if(OTPModel::verifyOTP('customer',$user->id,$request->type,$request->otp)){
-
+                $user->notification_token=$request->notification_token;
                 $user->status=1;
                 $user->save();
 
