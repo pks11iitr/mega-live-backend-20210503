6ac788b89d52e2a8bcfb10f22d3d4263152228b3
PankajVm <p.ks11iitr@gmail.com>
2021-02-03 11:57:00 +0530

    firebase notifications

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -16,7 +16,7 @@ class Customer extends Authenticatable implements JWTSubject
 
     protected $fillable = [
         'name', 'email', 'mobile', 'password', 'image', 'dob', 'address','country_id', 'city_id', 'state_id','pincode', 'status','notification_token', 'gender', 'education_id', 'occupation_id', 'employement_id', 'salaray_id', 'religion_id', 'height_id', 'language_id', 'marital_status_id', 'salary_id', 'about_me','image','height_feet', 'from_age', 'to_age','from_distance','to_distance','pref_gender'
-    ,'smoking','marijuana','drugs','drinking','job_id','ethicity_id', 'account_type', 'rate', 'country', 'country_flag'];
+    ,'smoking','marijuana','drugs','drinking','job_id','ethicity_id', 'account_type', 'rate', 'country', 'country_flag','age_show','distance_show'];
 
     protected $hidden = [
         'password','created_at','deleted_at','updated_at','email','mobile'
