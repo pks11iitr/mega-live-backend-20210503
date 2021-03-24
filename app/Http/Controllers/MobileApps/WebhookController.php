6dcd6f064e646fd626bc7f56bba1e30c128e2691
PankajVm <p.ks11iitr@gmail.com>
2021-03-24 16:27:49 +0530

    earnings controller in admin

--- app/Http/Controllers/MobileApps/WebhookController.php
+++ app/Http/Controllers/MobileApps/WebhookController.php
@@ -23,22 +23,23 @@ class WebhookController extends Controller
 
         if(isset($content['category']) && isset($content['direct_call']) && isset($content['direct_call']['call_id']) && isset($content['direct_call']['caller_id']) && isset($content['direct_call']['callee_id'])){
             if($content['category']=='direct_call:dial'){
-                $caller=$content['direct_call']['caller_id'];
-                $callee=$content['direct_call']['callee_id'];
+                $caller=str_replace('Matchon', '', $content['direct_call']['caller_id']);
+                $callee=str_replace('Matchon', '', $content['direct_call']['callee_id']);
 
-                $caller=Customer::where('sendbird_token', $caller)->first();
-                $callee=Customer::where('sendbird_token', $callee)->first();
-
-                CallRecord::create([
-                    'call_id'=>$content['direct_call']['call_id'],
-                    'caller_id'=>$caller->id,
-                    'callee_id'=>$callee->id,
-                    'type'=>'CALL',
-                    'minutes'=>0,
-                    'start'=>0,
-                    'end'=>0,
-                    'coins'=>0,
-                ]);
+                $caller=Customer::find($caller);
+                $callee=Customer::find($callee);
+                if($caller && $callee){
+                    CallRecord::create([
+                        'call_id'=>$content['direct_call']['call_id'],
+                        'caller_id'=>$caller->id,
+                        'callee_id'=>$callee->id,
+                        'type'=>'CALL',
+                        'minutes'=>0,
+                        'start'=>0,
+                        'end'=>0,
+                        'coins'=>0,
+                    ]);
+                }
             }else if($content['category']=='direct_call:accept'){
                 $call=CallRecord::where('call_id', $content['direct_call']['call_id'])->first();
 
