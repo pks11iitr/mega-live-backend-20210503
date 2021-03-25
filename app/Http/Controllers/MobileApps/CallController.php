072b3102a80f38ccfdf8452e5eba7d5647973c81
PankajVm <p.ks11iitr@gmail.com>
2021-03-25 10:44:55 +0530

    earnings controller in admin

--- app/Http/Controllers/MobileApps/CallController.php
+++ app/Http/Controllers/MobileApps/CallController.php
@@ -19,18 +19,30 @@ class CallController extends Controller
             $receiver=Customer::findOrFail($profile_id);
 
         //check balance for non admin users
-        if($user->account_type!='ADMIN')
-            if(CoinWallet::balance($user->id) < 50)
+        if($user->account_type!='ADMIN'){
+            $balance=CoinWallet::balance($user->id);
+            if( $balance < 50)
                 return [
                     'status'=>'failed',
                     'message'=>'recharge'
                 ];
+            $minutes=floor($balance/$receiver->rate);
+        }
+        else{
+            $balance=CoinWallet::balance($receiver->id);
+            if( $balance < 50)
+                return [
+                    'status'=>'failed',
+                    'message'=>'Low balance'
+                ];
+            $minutes=floor($balance/$user->rate);
+        }
 
         $user_id='Matchon'.$receiver->id;
 
         return [
             'status'=>'success',
-            'data'=>compact('user_id')
+            'data'=>compact('user_id', 'minutes')
         ];
 
     }
